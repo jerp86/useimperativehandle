67e72ed8a37d50075e42b5d37e1034b98b05d9d9
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-09-20 09:16:25 -0300

    created Profile component

---
+++ src/component/Profile/index.tsx
@@ -0,0 +1,27 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+import { memo } from "react";
+
+const Profile = () => (
+  <Flex>
+    <Flex alignItems="center">
+      <Image
+        src="https://github.com/jerp86.png"
+        alt="Profile pic"
+        width={48}
+        height={48}
+        borderRadius="full"
+      />
+    </Flex>
+
+    <Flex flexDirection="column" marginLeft={4}>
+      <Text fontSize="2xl" fontWeight="semibold" color="#3943B7">
+        José Pinto
+      </Text>
+      <Text fontSize="lg" fontWeight="medium" color="#717171">
+        Frontend Specialist
+      </Text>
+    </Flex>
+  </Flex>
+);
+
+export default memo(Profile);
