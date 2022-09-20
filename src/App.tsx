6b01fddcc73db7e99f0943b3793b77fd815c46f6
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-09-20 11:38:09 -0300

    add PreviousButton and NextButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { ChakraProvider, Flex, Text } from "@chakra-ui/react";
+import { Button, ChakraProvider, Flex, Image, Text } from "@chakra-ui/react";
+import { useCallback } from "react";
 
 import Gallery from "./component/Gallery";
 import Profile from "./component/Profile";
@@ -31,6 +32,52 @@ const images = [
 ];
 
 function App() {
+  const PreviousButton = useCallback(
+    () => (
+      <Button
+        onClick={() => {}}
+        background="transparent"
+        border="none"
+        width="50px"
+        height="50px"
+        fontSize="xl"
+        position="absolute"
+        left={0}
+        // disabled={hasNextPage}
+      >
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Chevron_left_font_awesome.svg/30px-Chevron_left_font_awesome.svg.png"
+          alt="Previous icon"
+          width="30px"
+        />
+      </Button>
+    ),
+    []
+  );
+
+  const NextButton = useCallback(
+    () => (
+      <Button
+        onClick={() => {}}
+        background="transparent"
+        border="none"
+        width="50px"
+        height="50px"
+        fontSize="xl"
+        position="absolute"
+        right={0}
+        // disabled={!hasNextPage}
+      >
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Chevron_right_font_awesome.svg/30px-Chevron_right_font_awesome.svg.png"
+          alt="Next icon"
+          width="30px"
+        />
+      </Button>
+    ),
+    []
+  );
+
   return (
     <ChakraProvider>
       <Flex
@@ -39,7 +86,7 @@ function App() {
         width="100%"
         height="100%"
       >
-        <Flex flexDirection="column">
+        <Flex flexDirection="column" width="xl">
           <Profile />
 
           <Text
@@ -51,7 +98,11 @@ function App() {
             Tech Stack
           </Text>
 
-          <Gallery images={images} />
+          <Gallery
+            images={images}
+            customNextButton={<NextButton />}
+            customPreviousButton={<PreviousButton />}
+          />
         </Flex>
       </Flex>
     </ChakraProvider>
