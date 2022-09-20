f4fcf1a77251ca7805aeb37815385406f67915ff
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-09-20 09:21:03 -0300

    add Flex and Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Text } from "@chakra-ui/react";
 
 import Profile from "./component/Profile";
 
 function App() {
   return (
     <ChakraProvider>
-      <Profile />
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        height="100%"
+      >
+        <Flex flexDirection="column">
+          <Profile />
+
+          <Text
+            marginTop={8}
+            marginBottom={4}
+            fontSize="2xl"
+            fontWeight="semibold"
+          >
+            Tech Stack
+          </Text>
+        </Flex>
+      </Flex>
     </ChakraProvider>
   );
 }
