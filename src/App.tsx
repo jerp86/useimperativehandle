5188efeb5deadb3c0c863ca235c355593cb1f440
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2022-09-20 10:45:49 -0300

    add Gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
 import { ChakraProvider, Flex, Text } from "@chakra-ui/react";
 
+import Gallery from "./component/Gallery";
 import Profile from "./component/Profile";
+
+const images = [
+  {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1200px-GrapQL_Logo.svg.png",
+  },
+  {
+    src: "https://relay.dev/img/relay.svg",
+  },
+  {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/1200px-Nextjs-logo.svg.png",
+  },
+  {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+  {
+    src: "https://relay.dev/img/relay.svg",
+  },
+  {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1200px-GrapQL_Logo.svg.png",
+  },
+  {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/1200px-Nextjs-logo.svg.png",
+  },
+  {
+    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  },
+];
 
 function App() {
   return (
@@ -22,6 +50,8 @@ function App() {
           >
             Tech Stack
           </Text>
+
+          <Gallery images={images} />
         </Flex>
       </Flex>
     </ChakraProvider>
